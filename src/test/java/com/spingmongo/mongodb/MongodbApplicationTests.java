66e6d9a4b744fdd4b96c5ade100473343309df82
carlos <carlosramos10k@gmail.com>
2023-08-02 10:31:27 -0300

    Configurações do projeto

---
+++ src/test/java/com/spingmongo/mongodb/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spingmongo.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
