b86ce3cd79c4dd0e522923c6da4aec029f35dac8
carlos <carlosramos10k@gmail.com>
2023-08-03 18:55:05 -0300

    relacionado usuario com seus posts

--- src/main/java/com/spingmongo/mongodb/config/Instantiation.java
+++ src/main/java/com/spingmongo/mongodb/config/Instantiation.java
@@ -44,5 +44,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("25/08/2023"), "Partiu slz", "Vou viajar para o Maranhão", new AuthorDto(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1,post2));
+        userRepository.save(maria);
     }
 }
